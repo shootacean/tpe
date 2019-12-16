bc23c8d2362c09b2c0a11037b72d4459c1260953
Shootacean <shootacean@gmail.com>
2019-12-16 23:55:54 +0900

    flutter tutorial step4

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,45 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-//    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Webcome to Flutter',
-      home: Scaffold(
-          appBar: AppBar(title: Text('Welcome to Flutter')),
-          body: Center(child: RandomWords())
-      ),
+      title: 'Startup Name GEnerator',
+      home: RandomWords(),
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Startup Name GEnerator'),
+        ),
+        body: _buildSuggestions());
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+        title: Text(
+      pair.asPascalCase,
+      style: _biggerFont,
+    ));
   }
 }
 
